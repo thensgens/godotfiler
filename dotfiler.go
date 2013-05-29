62ae7e872e2c5a50a90d7a4bf0561a09b11c56b3
Torben Hensgens <torben.hensgens@gmail.com>
2013-05-30 00:17:47 +0200

    basic implementation. see TODO for improvements.

--- dotfiler.go
+++ dotfiler.go
@@ -2,18 +2,51 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"path"
 )
 
 type Dotfiler struct {
 	option     FlagResult
 	exceptions []string
+	files      []string
 }
 
-func (self *Dotfiler) execute() error {
-	self.logStart()
-	return nil
+func (self *Dotfiler) execute() {
+	if self.option.verbose {
+		self.logStart()
+	}
+	for _, file := range self.files {
+		// these calls could be dispatched as goroutines later on
+		self.processElement(file)
+	}
+	if self.option.verbose {
+		self.logStop()
+	}
+}
+
+func (self *Dotfiler) processElement(file string) {
+	input := path.Join(self.option.source, file)
+	output := path.Join(self.option.target, file)
+
+	if self.option.verbose {
+		fmt.Printf("Copying %s  -->  %s\n", input, output)
+	}
+	b, err := ioutil.ReadFile(input)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(output, b, 0644)
+	if err != nil {
+		panic(err)
+	}
 }
 
 func (self *Dotfiler) logStart() {
-	fmt.Printf("Starting task [%s]\n", *self.option.name)
+	fmt.Printf("Started task [%s]\n", *self.option.name)
+}
+
+func (self *Dotfiler) logStop() {
+	fmt.Printf("Stopped task [%s]\n", *self.option.name)
 }
--- main.go
+++ main.go
@@ -3,48 +3,61 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
 )
 
 var (
 	default_source_dir = "/home/thens/Dropbox/linux/dotfiles"
 	default_target_dir = "/home/thens"
-	files              = []string{".bashrc", ".vimrc", ".tmux.conf"}
+
+	// just for testing purposes
+	// default_source_dir = "/home/thens/gocode/src/github.com/thensgens/godotfiler/test_files/source"
+	// default_target_dir = "/home/thens/gocode/src/github.com/thensgens/godotfiler/test_files/target"
+
+	// static configuration for now
+	files = []string{".bashrc", ".vimrc", ".tmux.conf"}
 )
 
 type FlagResult struct {
-	name   *string
-	source string
-	target string
+	name    *string
+	source  string
+	target  string
+	verbose bool
 }
 
 func checkFlags() FlagResult {
 	if len(os.Args) <= 1 {
 		usage()
 	}
+	flag.Usage = func() {
+		fmt.Println("Godotfiles has to be invoked with the --option flag.")
+		fmt.Println("Possible values are: restore / archive. Exiting..")
+		os.Exit(-1)
+	}
 	option := flag.String("option", "restore", "Option can either be restore or archive.")
+	verbose_long := flag.Bool("verbose", false, "Verbose command flag.")
+	verbose_short := flag.Bool("v", false, "Verbose command flag.")
 	flag.Parse()
+	verbose := *verbose_long || *verbose_short
+
 	var result FlagResult
 	if *option == "restore" {
-		result := FlagResult{name: option, source: default_source_dir, target: default_target_dir}
+		result = FlagResult{name: option, source: default_source_dir, target: default_target_dir, verbose: verbose}
 	} else {
-		result := FlagResult{name: option, source: default_target_dir, target: default_source_dir}
+		result = FlagResult{name: option, source: default_target_dir, target: default_source_dir, verbose: verbose}
 	}
 	return result
 }
 
 func usage() {
-	fmt.Println("Godotfiles has to be invoked with the -option flag.")
-	fmt.Println("Possible values are: restore / archive")
-	log.Fatal("Exiting..")
+	fmt.Println("Godotfiles has to be invoked with the --option flag.")
+	fmt.Println("Possible values are: restore / archive. Exiting..")
+	os.Exit(-1)
 }
 
 func main() {
+	// sanity cli flag checks
 	flagOption := checkFlags()
-	dotfiler := &Dotfiler{option: flagOption}
-	err := dotfiler.execute()
-	if err != nil {
-		log.Fatalf("Error occured: %s\nExiting..", err)
-	}
+	dotfiler := &Dotfiler{option: flagOption, files: files}
+	dotfiler.execute()
 }
